26426bc6c63179209218815860162bee79318002
Mridula <mridulas.aids2024@citchennai.net>
2026-01-25 23:45:26 +0530

    fix eslint unused vars

--- Portfolio-master/src/components/About/Toolstack.js
+++ Portfolio-master/src/components/About/Toolstack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import macOs from "../../Assets/TechIcons/Apple MacOSX.svg";
 import chrome from "../../Assets/TechIcons/Google Chrome.svg";
 import vsCode from "../../Assets/TechIcons/vscode.svg";
 import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
@@ -15,11 +14,6 @@ function Toolstack() {
       <Col xs={4} md={2} className="tech-icons ">
         <img src={vsCode} alt="vsCode" className="tech-icon-images" />
         <div className="tech-icons-text">Vs Code</div>
-      </Col>
-
-      <Col xs={4} md={2} className="tech-icons ">
-        <img src={intelliJ} alt="go" className="tech-icon-images" />
-        <div className="tech-icons-text">IntelliJ</div>
       </Col>
     </Row>
   );
