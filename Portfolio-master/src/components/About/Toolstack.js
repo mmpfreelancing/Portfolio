4e95e8deebcc8f3cac43008bc6a27c3eaab69176
Mridula <mridulas.aids2024@citchennai.net>
2026-01-26 11:00:04 +0530

    fix remaining unused vars

--- Portfolio-master/src/components/About/Toolstack.js
+++ Portfolio-master/src/components/About/Toolstack.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import chrome from "../../Assets/TechIcons/Google Chrome.svg";
 import vsCode from "../../Assets/TechIcons/vscode.svg";
-import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
 
 function Toolstack() {
   return (
